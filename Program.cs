5bb3533aed95b27d7981f5f39de3c704c75ca24f
Pankaj Puri <pankajpuri016@gmail.com>
2025-02-28 08:22:57 +1300

    TextCopy is installed and created methods to generate and copy ticket.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,66 @@
-﻿namespace GenerateGItBranchFromAzureDevOpsTicket
+﻿
+using System.Windows;
+using TextCopy;
+
+namespace GenerateGItBranchFromAzureDevOpsTicket
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("Enter the ticket number.");
+                var ticket = Console.ReadLine();
+
+                Console.WriteLine();
+
+                int ticketLength = ticket.Length;
+
+
+                PrintDash(ticketLength);
+
+                GenrateGitBranch(ticket, ticketLength);
+            }
+        }
+
+        private static void PrintDash(int lengthOfTicket)
+        {
+            for (int i = 0; i < lengthOfTicket; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("");
+        }
+
+        private static void GenrateGitBranch(string? ticket, int ticketLength)
+        {
+            if (ticket != null)
+            {
+                if (ticket.Contains(":"))
+                {
+                    ticket = ticket.Replace(":", " ");
+                }
+                if (ticket.Contains("["))
+                {
+                    var findSqBracketIndex = ticket.IndexOf('[');
+                    ticket = ticket.Remove(findSqBracketIndex - 1);
+                }
+
+                var firstSpaceIndex = ticket.IndexOf(" ");
+                ticket = ticket.Insert(firstSpaceIndex, "/ab#");
+                ticket = ticket.Replace(' ', '-');
+
+            }
+            else
+            {
+                return;
+            }
+            var printTicket = "git checkout -b \"" + ticket + "\"";
+            Console.WriteLine(printTicket);
+            PrintDash(ticketLength);
+            ClipboardService.SetText(printTicket);
+            Console.WriteLine();
         }
     }
 }
